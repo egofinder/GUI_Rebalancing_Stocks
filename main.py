d5c52aa8f0572e11546fe299255a080b06839681
Benjamin Jeon <baekhakjeon@gmail.com>
2021-02-21 01:59:09 -0800

    Message Box added, Rebalancing Button added.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from tkinter import *
-import tkinter.ttk as ttk # Library for Prograssive Bar library
+from tkinter import messagebox # Library for messagebox
+import tkinter.ttk as ttk # Library for Prograssive Bar
 from tkinter import filedialog # Library for CSV File opening
 from pandastable import Table # Library to disply Pandas DataFrame to Tkinter
 from yahoo_fin.stock_info import tickers_sp500
@@ -29,16 +30,21 @@ class RebalancingApp(Frame):
         self.menu_file.add_separator()
         self.menu_file.add_command(label="Save to Excel", state="disable") # 비활성화
         self.menu_file.add_separator()
-        self.menu_file.add_command(label="Exit", command=self.main.quit)
+        self.menu_file.add_command(label="Exit", command=self.quit)
         self.menu.add_cascade(label="File", menu=self.menu_file)
 
         # Strategies Menu
+        self.s_var = IntVar()
         self.menu_strategies = Menu(self.menu, tearoff=0)
-        self.menu_strategies.add_radiobutton(label="Equal weight")
-        self.menu_strategies.add_radiobutton(label="Market Cap")
-        self.menu_strategies.add_radiobutton(label="P/E Ratio")
+        self.menu_strategies.add_radiobutton(label="Equal weight", variable=self.s_var, value=1)
+        self.menu_strategies.add_radiobutton(label="Market Cap", variable=self.s_var, value=2)
+        self.menu_strategies.add_radiobutton(label="P/E Ratio", variable=self.s_var, value=3)
         # Default Strategies menu disable since there is no data to calculate
         self.menu.add_cascade(label="Strategies", menu=self.menu_strategies, state="disable") 
+
+        # Button to print out Strategies data
+        self.button_rebalancing = Button(self.main, text='Rebalancing', command=self.rebalancing, state="disable")
+        self.button_rebalancing.pack(padx=1, pady=1, fill='x', side='top')
 
         # Add menu bar to main Window
         self.main.config(menu=self.menu)
@@ -48,17 +54,54 @@ class RebalancingApp(Frame):
         self.display.pack(padx=1, pady=1, fill=BOTH, side='top', expand=1)
         
         # Display Progressbar to bottom at Window
-        self.p_var2 = DoubleVar()
-        self.progressbar2 = ttk.Progressbar(self.main, maximum=100, variable=self.p_var2)
-        self.progressbar2.pack(padx=1, pady=1, side='bottom', fill='x')
+        self.p_var = DoubleVar()
+        self.progressbar = ttk.Progressbar(self.main, maximum=100, variable=self.p_var)
+        self.progressbar.pack(padx=1, pady=1, side='bottom', fill='x')
         
 
 # Functions
-    def load_market_data(self) -> 'pandas.DataFrame':
+    # Window X Button overide
+    # def on_closing():
+    #     if messagebox.askokcancel("Quit", "Do you want to quit?"):
+    #         root.destroy()
+
+    def quit(self):
+        if messagebox.askyesno("Exit","Do you want to quit the application?"):
+            self.main.quit()
+
+    def rebalancing(self):
+        
+        if self.s_var.get() == 0:
+            messagebox.showerror("Error","Please, select your strategy!")
+        
+        elif self.s_var.get() == 1:
+            self.equal_rebalancing()
+            
+        elif self.s_var.get() == 2:
+            self.market_cap_rebalancing()
+            
+        elif self.s_var.get() == 3:
+            self.pe_ratio_rebalancing()
+        
+        else:
+            messagebox.showerror("Critical","Something wrong!!!!!!!!")
+        
+# Rabalancing Functions    
+    def equal_rebalancing(self):
+        print('Equal')
+        
+    def market_cap_rebalancing(self):
+        print('Market Cap')
+        
+    def pe_ratio_rebalancing(self):
+        print('P/E Ratio')  
+        
+         
+    def load_market_data(self):
         self.tickers = tickers_sp500(True)
         self.tickers.set_index('Symbol', inplace=True)
         
-    def load_user_data(self) -> 'pandas.DataFrame':
+    def load_user_data(self):
         _file = filedialog.askopenfilename(title="Choose csv file...",  \
             filetypes=(("csv", "*.csv"),("All", "*.*")), initialdir=r"C:\Users\baekh\OneDrive\Desktop\Python")
         try:
@@ -67,7 +110,7 @@ class RebalancingApp(Frame):
             # Keep only first row if there is duplications
             self.df.drop_duplicates(subset=['Symbol'], inplace=True)
             self.df.set_index('Symbol', inplace=True)
-            # self.df = self.df.head(5)
+            self.df = self.df.head(5)
             self.pt = Table(self.display, dataframe=self.df, showtoolbar=False, showstatusbar=False)
             self.pt.autoResizeColumns()
             self.pt.show()
@@ -77,12 +120,12 @@ class RebalancingApp(Frame):
         except:
             pass
              
-    def update_data(self) -> 'pandas.DataFrame':
+    def update_data(self):
         self.load_market_data()
         for i, symbol in enumerate(self.df.index):
             # Variable for display status of Progressive Bar
-            self.p_var2.set(100/len(self.df.index)*(i+1))
-            self.progressbar2.update()
+            self.p_var.set(100/len(self.df.index)*(i+1))
+            self.progressbar.update()
 
             if symbol in self.tickers.index:
                 # Loading live market data from yahoo finance
@@ -113,6 +156,7 @@ class RebalancingApp(Frame):
         # Update menu status
         self.menu.entryconfig("Strategies", state="normal") 
         self.menu_file.entryconfig("Save to Excel", state="normal")
+        self.button_rebalancing.config(state="normal")
 
 app = RebalancingApp()
 app.mainloop()
